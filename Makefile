8f9b26b2134582116c4e082114fec131ecbda886
Sam Kravitz <samkravitz99@gmail.com>
2021-07-26 18:46:50 -0400

    Follow tutorial for custom bootloader

--- Makefile
+++ Makefile
@@ -1,33 +1,27 @@
-AS = ${HOME}/opt/cross/bin/i686-elf-as
+AS = nasm
 CC = ${HOME}/opt/cross/bin/i686-elf-gcc
 
-C_SOURCES = $(wildcard *.c)
-ASM_SOURCES = $(wildcard *.s)
+C_SOURCES = main.c
+ASM_SOURCES = boot.s
 
 OBJ = ${C_SOURCES:.c=.o} ${ASM_SOURCES:.s=.o}
 
-all: myos.iso
+all: kernel.bin
 
 %.o: %.c
 	${CC} -c $< -o $@ -std=c99 -ffreestanding -O2 -Wall -Wextra
 
 %.o: %.s
-	${AS} $< -o $@
+	${AS} -f elf32 $< -o $@
 
-myos.bin: ${OBJ}
+kernel.bin: ${OBJ}
 	${CC} -T linker.ld -o $@ -ffreestanding -O2 -nostdlib $^ -lgcc
 
-check-multiboot: myos.bin
-	grub-file --is-x86-multiboot myos.bin
-
-myos.iso: check-multiboot
-	rm -rf isodir/
-	mkdir -p isodir/boot/grub
-	cp myos.bin isodir/boot/myos.bin
-	cp grub.cfg isodir/boot/grub/grub.cfg
-	grub-mkrescue -o myos.iso isodir
+.PHONY: start
+start:
+	qemu-system-i386 -fda kernel.bin
 
 
+.PHONY: clean
 clean:
-	rm -rf isodir/
-	rm -rf *.o *.bin *.iso
+	rm -rf *.o *.bin
--- boot.s
+++ boot.s
@@ -1,109 +1,88 @@
-/* Declare constants for the multiboot header. */
-.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
-.set MEMINFO,  1<<1             /* provide memory map */
-.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
-.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
-.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
- 
-/* 
-Declare a multiboot header that marks the program as a kernel. These are magic
-values that are documented in the multiboot standard. The bootloader will
-search for this signature in the first 8 KiB of the kernel file, aligned at a
-32-bit boundary. The signature is in its own section so the header can be
-forced to be within the first 8 KiB of the kernel file.
-*/
-.section .multiboot
-.align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
- 
-/*
-The multiboot standard does not define the value of the stack pointer register
-(esp) and it is up to the kernel to provide a stack. This allocates room for a
-small stack by creating a symbol at the bottom of it, then allocating 16384
-bytes for it, and finally creating a symbol at the top. The stack grows
-downwards on x86. The stack is in its own section so it can be marked nobits,
-which means the kernel file is smaller because it does not contain an
-uninitialized stack. The stack on x86 must be 16-byte aligned according to the
-System V ABI standard and de-facto extensions. The compiler will assume the
-stack is properly aligned and failure to align the stack will result in
-undefined behavior.
-*/
-.section .bss
-.align 16
-stack_bottom:
-.skip 16384 # 16 KiB
-stack_top:
- 
-/*
-The linker script specifies _start as the entry point to the kernel and the
-bootloader will jump to this position once the kernel has been loaded. It
-doesn't make sense to return from this function as the bootloader is gone.
-*/
-.section .text
-.global _start
-.type _start, @function
-_start:
-	/*
-	The bootloader has loaded us into 32-bit protected mode on a x86
-	machine. Interrupts are disabled. Paging is disabled. The processor
-	state is as defined in the multiboot standard. The kernel has full
-	control of the CPU. The kernel can only make use of hardware features
-	and any code it provides as part of itself. There's no printf
-	function, unless the kernel provides its own <stdio.h> header and a
-	printf implementation. There are no security restrictions, no
-	safeguards, no debugging mechanisms, only what the kernel provides
-	itself. It has absolute and complete power over the
-	machine.
-	*/
- 
-	/*
-	To set up a stack, we set the esp register to point to the top of the
-	stack (as it grows downwards on x86 systems). This is necessarily done
-	in assembly as languages such as C cannot function without a stack.
-	*/
-	mov $stack_top, %esp
- 
-	/*
-	This is a good place to initialize crucial processor state before the
-	high-level kernel is entered. It's best to minimize the early
-	environment where crucial features are offline. Note that the
-	processor is not fully initialized yet: Features such as floating
-	point instructions and instruction set extensions are not initialized
-	yet. The GDT should be loaded here. Paging should be enabled here.
-	C++ features such as global constructors and exceptions will require
-	runtime support to work as well.
-	*/
- 
-	/*
-	Enter the high-level kernel. The ABI requires the stack is 16-byte
-	aligned at the time of the call instruction (which afterwards pushes
-	the return pointer of size 4 bytes). The stack was originally 16-byte
-	aligned above and we've pushed a multiple of 16 bytes to the
-	stack since (pushed 0 bytes so far), so the alignment has thus been
-	preserved and the call is well defined.
-	*/
-	call kernel_main
- 
-	/*
-	If the system has nothing more to do, put the computer into an
-	infinite loop. To do that:
-	1) Disable interrupts with cli (clear interrupt enable in eflags).
-	   They are already disabled by the bootloader, so this is not needed.
-	   Mind that you might later enable interrupts and return from
-	   kernel_main (which is sort of nonsensical to do).
-	2) Wait for the next interrupt to arrive with hlt (halt instruction).
-	   Since they are disabled, this will lock up the computer.
-	3) Jump to the hlt instruction if it ever wakes up due to a
-	   non-maskable interrupt occurring or due to system management mode.
-	*/
+; this bootloader code was graciously taken from:
+; http://3zanders.co.uk/2017/10/18/writing-a-bootloader3/
+
+section .boot
+bits 16
+global boot
+boot:
+	mov ax, 0x2401
+	int 0x15
+
+	mov ax, 0x3
+	int 0x10
+
+	mov [disk],dl
+
+	mov ah, 0x2    ;read sectors
+	mov al, 6      ;sectors to read
+	mov ch, 0      ;cylinder idx
+	mov dh, 0      ;head idx
+	mov cl, 2      ;sector idx
+	mov dl, [disk] ;disk idx
+	mov bx, copy_target;target pointer
+	int 0x13
 	cli
-1:	hlt
-	jmp 1b
- 
-/*
-Set the size of the _start symbol to the current location '.' minus its start.
-This is useful when debugging or when you implement call tracing.
-*/
-.size _start, . - _start
+	lgdt [gdt_pointer]
+	mov eax, cr0
+	or eax,0x1
+	mov cr0, eax
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	jmp CODE_SEG:boot2
+gdt_start:
+	dq 0x0
+gdt_code:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10011010b
+	db 11001111b
+	db 0x0
+gdt_data:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+gdt_end:
+gdt_pointer:
+	dw gdt_end - gdt_start
+	dd gdt_start
+disk:
+	db 0x0
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+copy_target:
+bits 32
+	hello: db "Hello more than 512 bytes world!!",0
+boot2:
+	mov esi,hello
+	mov ebx,0xb8000
+.loop:
+	lodsb
+	or al,al
+	jz halt
+	or eax,0x0F00
+	mov word [ebx], ax
+	add ebx,2
+	jmp .loop
+halt:
+	mov esp,kernel_stack_top
+	extern kmain
+	call kmain
+	cli
+	hlt
+
+section .bss
+align 4
+kernel_stack_bottom: equ $
+	resb 16384 ; 16 KB
+kernel_stack_top:
