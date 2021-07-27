1d71502388bbf5f1145161b466f1ef8dc7a0987f
Sam Kravitz <samkravitz99@gmail.com>
2021-07-26 21:41:16 -0400

    Add common.h

---
+++ include/common.h
@@ -0,0 +1,25 @@
+/* kernel
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * Author: Sam Kravitz
+ *
+ * FILE: common.h
+ * DATE: July 26, 2021
+ * DESCRIPTION: Common use typedefs
+ */
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+#endif // COMMON_H
