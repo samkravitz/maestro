bf489dcfb20bd44df4a9a3f5b2c8041f1d2e2b8f
Sam Kravitz <samkravitz99@gmail.com>
2021-07-26 22:03:21 -0400

    Change main.c to kmain.c

---
+++ src/kmain.c
@@ -0,0 +1,18 @@
+/* kernel
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * Author: Sam Kravitz
+ *
+ * FILE: kmain.c
+ * DATE: July 26, 2021
+ * DESCRIPTION: Where it all begins
+ */
+#include <stdint.h>
+
+#include "txtmode.h"
+
+void kmain()
+{
+    const char *hello = "Hello\nWorld!\n";
+    txtmode_puts(hello);
+}
