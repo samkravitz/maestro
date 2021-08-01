d8322a39f3b88ebfebcf7402cf29a12762da3388
Sam Kravitz <samkravitz99@gmail.com>
2021-08-01 11:56:27 -0400

    Adds stdlib.c

---
+++ lib/libc/stdlib.c
@@ -0,0 +1,52 @@
+#include "stdlib.h"
+
+static void rev(char *, int);
+
+// itoa - converts a number into a string
+char *itoa(int n, char *buff, int base)
+{
+	char c;
+	int mod, i = 0;
+
+	int neg = 0;
+	if ((base == 10) && (n < 0))
+	{
+		neg = 1;
+		n *= -1;
+	}
+
+	while (n)
+	{
+		mod = n % base;
+
+		if (mod >= 10)
+			c = 'a' + (mod - 10);
+		
+		else
+			c = '0' + mod;
+		
+		buff[i++] = c;
+		n /= base;
+	}
+
+	if (neg)
+		buff[i++] = '-';
+
+	rev(buff, i);
+	return buff;
+}
+
+// rev - reverses a string of a given length
+static void rev(char *str, int len)
+{
+	if (len <= 0)
+		return;
+
+	char temp[len];
+
+	for (int i = 0; i < len; ++i)
+		temp[i] = str[len - i - 1];
+
+	for (int i = 0; i < len; ++i)
+		str[i] = temp[i];
+}
---
+++ lib/libc/stdlib.h
@@ -0,0 +1,6 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+
+char *itoa(int, char *, int);
+
+#endif // STDLIB_H
