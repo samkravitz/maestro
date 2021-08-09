b0ecc4d1487579ccee73e39f6ef18a1a7d5de3c1
Sam Kravitz <samkravitz99@gmail.com>
2021-08-09 17:34:13 -0400

    Update kmain

--- src/kmain.c
+++ src/kmain.c
@@ -9,12 +9,32 @@
  */
 #include <init.h>
 #include <kout.h>
+#include <proc.h>
+#include <pq.h>
+
+#include "malloc.h"
+#include "string.h"
+
+extern struct pq *rdylist;
 
 void kmain()
 {
 	kout("Welcome to maestro!\n");
 	init();
 
-	// hang so interrupts remain enabled
-	while (1);
+	// set up null process
+	struct proc *null = (struct proc *) malloc(sizeof(struct proc));
+	u32 stkptr = (u32) malloc(1024);
+	null->pid = 0;
+	null->prio = -20;
+	null->stkbase = stkptr;
+	null->stkptr = stkptr;
+	null->prstate = PR_READY;
+	memcpy(null->name, "null process\0", strlen("null process") + 1);
+
+	// create ready list
+	rdylist = newpq(null);
+
+	while (1)
+		asm("hlt");
 }
