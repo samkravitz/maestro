014e24c9694142cf62f1854f40a60f041eff9655
Sam Kravitz <samkravitz99@gmail.com>
2021-08-09 17:33:32 -0400

    Add cli and sti functions

--- include/intr.h
+++ include/intr.h
@@ -12,24 +12,24 @@
 
 #include <state.h>
 
-#define NUM_INTERRUPTS 256
+#define NUM_INTERRUPTS 		256
 
-#define IRQ0	 32 // Programmable Interrupt Timer Interrupt
-#define IRQ1	 33 // Keyboard Interrupt
-#define IRQ2	 34 // Cascade (used internally by the two PICs. never raised)
-#define IRQ3	 35 // COM2 (if enabled)
-#define IRQ4	 36 // COM1 (if enabled)
-#define IRQ5	 37 // LPT2 (if enabled)
-#define IRQ6	 38 // Floppy Disk
-#define IRQ7	 39 // LPT1 / Unreliable "spurious" interrupt (usually)
-#define IRQ8	 40 // CMOS real-time clock (if enabled)
-#define IRQ9	 41 // Free for peripherals / legacy SCSI / NIC
-#define IRQ10	 42 // Free for peripherals / SCSI / NIC
-#define IRQ11	 43 // Free for peripherals / SCSI / NIC
-#define IRQ12	 44 // PS2 Mouse
-#define IRQ13	 45 // FPU / Coprocessor / Inter-processor
-#define IRQ14	 46 // Primary ATA Hard Disk
-#define IRQ15	 47 // Secondary ATA Hard Disk
+#define IRQ0	 32			// Programmable Interrupt Timer Interrupt
+#define IRQ1	 33			// Keyboard Interrupt
+#define IRQ2	 34			// Cascade (used internally by the two PICs. never raised)
+#define IRQ3	 35			// COM2 (if enabled)
+#define IRQ4	 36			// COM1 (if enabled)
+#define IRQ5	 37			// LPT2 (if enabled)
+#define IRQ6	 38			// Floppy Disk
+#define IRQ7	 39			// LPT1 / Unreliable "spurious" interrupt (usually)
+#define IRQ8	 40			// CMOS real-time clock (if enabled)
+#define IRQ9	 41			// Free for peripherals / legacy SCSI / NIC
+#define IRQ10	 42			// Free for peripherals / SCSI / NIC
+#define IRQ11	 43			// Free for peripherals / SCSI / NIC
+#define IRQ12	 44			// PS2 Mouse
+#define IRQ13	 45			// FPU / Coprocessor / Inter-processor
+#define IRQ14	 46			// Primary ATA Hard Disk
+#define IRQ15	 47			// Secondary ATA Hard Disk
 
 // holds registered interrupt handlers
 extern void (*intr_handlers[NUM_INTERRUPTS]) (void);
@@ -40,6 +40,10 @@ void irq(int);
 void isr(struct state);
 void panic(struct state);
 void regint(int, void (*)(void));
+
+// disable / enable interrupts
+static void cli() { asm("cli"); }
+static void sti() { asm("sti"); }
 
 // exception messages
 static const char *excmsg[] = {
