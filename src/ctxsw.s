363034b13fba29f023b01f3e1441c85f24f072cc
Sam Kravitz <samkravitz99@gmail.com>
2021-08-11 10:30:07 -0400

    ctxsw

--- src/ctxsw.s
+++ src/ctxsw.s
@@ -2,16 +2,18 @@
 
 global ctxsw:
 ctxsw:
-    push ebp
-    mov ebp, esp
-    pushf
-    pusha
-    mov eax, [ebp + 8]
-    mov [eax], esp
-    mov eax, [ebp + 12]
-    mov esp, [eax]
-    popa
-    mov ebp, [esp + 4]
-    popf
     add esp, 4
     ret
+    ; push ebp
+    ; mov ebp, esp
+    ; pushf
+    ; pusha
+    ; mov eax, [ebp + 8]
+    ; mov [eax], esp
+    ; mov eax, [ebp + 12]
+    ; mov esp, [eax]
+    ; popa
+    ; mov ebp, [esp + 4]
+    ; popf
+    ; add esp, 4
+    ; ret
