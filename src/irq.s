86a1549d16c3a29dfe74dd5f69b458c373311c1b
Sam Kravitz <samkravitz99@gmail.com>
2021-08-11 20:56:25 -0400

    Remove unnecessary sti/cli in interrupt handler code

--- src/irq.s
+++ src/irq.s
@@ -12,97 +12,81 @@ extern irq
 
 global irq0
 irq0:
-	cli				; disable interrupts
 	mov eax, 32		; hold irq number so dispatcher can identify it
 	jmp irq_disp	; jump to common irq dispatcher
 
 global irq1
 irq1:
-	cli
 	mov eax, 33
 	jmp irq_disp
 
 global irq2
 irq2:
-	cli
 	mov eax, 34
 	jmp irq_disp
 
 global irq3
 irq3:
-	cli
 	mov eax, 35
 	jmp irq_disp
 
 global irq4
 irq4:
-	cli
 	mov eax, 36
 	jmp irq_disp
 
 global irq5
 irq5:
-	cli
 	mov eax, 37
 	jmp irq_disp
 
 global irq6
 irq6:
-	cli
 	mov eax, 38
 	jmp irq_disp
 
 global irq7
 irq7:
-	cli
 	mov eax, 39
 	jmp irq_disp
 
 global irq8
 irq8:
-	cli
 	mov eax, 40
 	jmp irq_disp
 
 global irq9
 irq9:
-	cli
 	mov eax, 41
 	jmp irq_disp
 
 global irq10
 irq10:
-	cli
 	mov eax, 42
 	jmp irq_disp
 
 global irq11
 irq11:
-	cli
 	mov eax, 43
 	jmp irq_disp
 
 global irq12
 irq12:
-	cli
 	mov eax, 44
 	jmp irq_disp
 
 global irq13
 irq13:
-	cli
 	mov eax, 45
 	jmp irq_disp
 
 global irq14
 irq14:
-	cli
 	mov eax, 46
 	jmp irq_disp
 
 global irq15
 irq15:
-	cli
 	mov eax, 47
 	jmp irq_disp
 
@@ -113,5 +97,4 @@ irq_disp:
 	call irq	; call the registered irq handler
 	pop eax		; clean up stack
 	popa		; restore state
-	sti			; restore interrupts
 	iret 		; return from interrupt
--- src/isr.s
+++ src/isr.s
@@ -15,218 +15,186 @@ extern isr
 
 global isr0
 isr0:
-	cli				; disable interrupts
 	push 0			; push dummy error code
 	push 0			; push interrupt number
 	jmp isr_disp	; jump to isr dispatcher
 
 global isr1
 isr1:
-	cli
 	push 0
 	push 1
 	jmp isr_disp
 
 global isr2
 isr2:
-	cli
 	push 0
 	push 2
 	jmp isr_disp
 
 global isr3
 isr3:
-	cli
 	push 0
 	push 3
 	jmp isr_disp
 
 global isr4
 isr4:
-	cli
 	push 0
 	push 4
 	jmp isr_disp
 
 global isr5
 isr5:
-	cli
 	push 0
 	push 5
 	jmp isr_disp
 
 global isr6
 isr6:
-	cli
 	push 0
 	push 6
 	jmp isr_disp
 
 global isr7
 isr7:
-	cli
 	push 0
 	push 7
 	jmp isr_disp
 
 global isr8
 isr8:
-	cli
 	push 8
 	jmp isr_disp
 
 global isr9
 isr9:
-	cli
 	push 0
 	push 9
 	jmp isr_disp
 
 global isr10
 isr10:
-	cli
 	push 10
 	jmp isr_disp
 
 global isr11
 isr11:
-	cli
 	push 11
 	jmp isr_disp
 
 global isr12
 isr12:
-	cli
 	push 12
 	jmp isr_disp
 
 global isr13
 isr13:
-	cli
 	push 13
 	jmp isr_disp
 
 global isr14
 isr14:
-	cli
 	push 14
 	jmp isr_disp
 
 global isr15
 isr15:
-	cli
 	push 0
 	push 15
 	jmp isr_disp
 
 global isr16
 isr16:
-	cli
 	push 0
 	push 16
 	jmp isr_disp
 
 global isr17
 isr17:
-	cli
 	push 0
 	push 17
 	jmp isr_disp
 
 global isr18
 isr18:
-	cli
 	push 0
 	push 18
 	jmp isr_disp
 
 global isr19
 isr19:
-	cli
 	push 0
 	push 19
 	jmp isr_disp
 
 global isr20
 isr20:
-	cli
 	push 0
 	push 20
 	jmp isr_disp
 
 global isr21
 isr21:
-	cli
 	push 0
 	push 21
 	jmp isr_disp
 
 global isr22
 isr22:
-	cli
 	push 0
 	push 22
 	jmp isr_disp
 
 global isr23
 isr23:
-	cli
 	push 0
 	push 23
 	jmp isr_disp
 
 global isr24
 isr24:
-	cli
 	push 0
 	push 24
 	jmp isr_disp
 
 global isr25
 isr25:
-	cli
 	push 0
 	push 25
 	jmp isr_disp
 
 global isr26
 isr26:
-	cli
 	push 0
 	push 26
 	jmp isr_disp
 
 global isr27
 isr27:
-	cli
 	push 0
 	push 27
 	jmp isr_disp
 
 global isr28
 isr28:
-	cli
 	push 0
 	push 28
 	jmp isr_disp
 
 global isr29
 isr29:
-	cli
 	push 0
 	push 29
 	jmp isr_disp
 
 global isr30
 isr30:
-	cli
 	push 0
 	push 30
 	jmp isr_disp
 
 global isr31
 isr31:
-	cli
 	push 0
 	push 31
 	jmp isr_disp
@@ -237,5 +205,4 @@ isr_disp:
    	call isr		; call isr dispatcher
    	popa
    	add esp, 8		; clean up stack
-   	sti				; enable interrupts
    	iret
