a43e70eb05066830e121b4fbe4154acd954b7729
Sam Kravitz <samkravitz99@gmail.com>
2021-08-26 00:41:31 -0400

    Implement atoi

--- lib/libc/stdlib.c
+++ lib/libc/stdlib.c
@@ -2,6 +2,34 @@
 
 static void rev(char *, int);
 
+// converts a string into a number
+int atoi(const char *str)
+{
+	int neg = 0;
+	if (*str == '-')
+	{
+		neg = 1;
+		str++;
+	}
+
+	int num = 0;
+
+	while (*str && is_numeric(*str))
+	{
+		num += *str - '0';
+		str++;
+		num *= 10;
+	}
+
+	// account for multiplying by 10 one too many times
+	num /= 10;
+
+	if (neg)
+		num *= -1;
+	
+	return num;
+}
+
 // converts a number into a string
 char *itoa(u32 n, char *buff, int base)
 {
--- lib/libc/stdlib.h
+++ lib/libc/stdlib.h
@@ -3,6 +3,8 @@
 
 #include <maestro.h>
 
+#define is_numeric(c)	((c >= '0') && (c <= '9'))
+int atoi(const char *);
 char *itoa(u32, char *, int);
 
 #endif // STDLIB_H
