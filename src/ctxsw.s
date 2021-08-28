e1e988447bd0124d936692c5a757b9301305c71d
Sam Kravitz <38745575+samkravitz@users.noreply.github.com>
2021-08-27 23:24:28 -0400

    Update ctxsw.s

--- src/ctxsw.s
+++ src/ctxsw.s
@@ -19,7 +19,7 @@ ctxsw:
 	push esi
 	push edi
 	mov [eax], esp      		; save old stack
-	mov esp, ecx
+	mov esp, ecx			; move new stack into esp
 	pop edi				; restore callee-saved registers of new process
 	pop esi
 	pop ebx
