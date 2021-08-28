0457b5f8fd282ba6a278f21a190c50f42be2a7d6
Sam Kravitz <38745575+samkravitz@users.noreply.github.com>
2021-08-27 23:21:15 -0400

    Fix styling

--- src/ctxsw.s
+++ src/ctxsw.s
@@ -11,18 +11,18 @@
 ; cdecl - void ctxsw(void *oldstk, void *newstk)
 global ctxsw:
 ctxsw:
-	mov eax, [esp + 4]  ; old process
-    mov ecx, [esp + 8]  ; new process
+	mov eax, [esp + 4]		; old process
+	mov ecx, [esp + 8]		; new process
 	pushf				; save callee-saved registers
-    push ebp
-    push ebx
-    push esi
-    push edi
-    mov [eax], esp      ; save old stack
-    mov esp, ecx
-    pop edi				; restore callee-saved registers of new process
-    pop esi
-    pop ebx
-    pop ebp
+	push ebp
+	push ebx
+	push esi
+	push edi
+	mov [eax], esp      		; save old stack
+	mov esp, ecx
+	pop edi				; restore callee-saved registers of new process
+	pop esi
+	pop ebx
+	pop ebp
 	popf
-    ret
+	ret
