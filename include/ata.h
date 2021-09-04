96a97e41d0e390f8dad9822aa2cb97816956a581
Sam Kravitz <samkravitz99@gmail.com>
2021-09-03 20:03:00 -0400

    Fix bad disk driver polling

--- include/ata.h
+++ include/ata.h
@@ -15,6 +15,9 @@
 #define ATA_CMD_READ			0x20
 #define ATA_CMD_WRITE			0x30
 
+#define ATA_WAIT_BSY			0x80
+#define ATA_WAIT_RDY			0x40
+
 #define ATA_DATA_PORT			0x1f0
 #define ATA_SECTOR_COUNT_PORT	0x1f2
 #define ATA_LBA_LOW_PORT		0x1f3
@@ -23,12 +26,6 @@
 #define ATA_LBA_PORT			0x1f6 
 #define ATA_CMD_PORT			0x1f7	// used to write commands to the disk
 #define ATA_STATUS_PORT			0x1f7	// used to read status from the disk
-
-enum ata_wait_occasion
-{
-	ATA_WAIT_RDY = 1 << 6,
-	ATA_WAIT_BSY = 1 << 7,	// we invert this because we want to wait until this bit is 0
-} __attribute__((packed));
 
 void ata_read(void *, uint, size_t);
 void ata_write(void *, uint, size_t);
