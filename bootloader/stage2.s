18426869659aecc23306b02bf4f13296cbccc7c5
Sam Kravitz <samkravitz99@gmail.com>
2022-03-04 18:05:20 -0500

    Add routine to check if a20 line is enabled

--- bootloader/stage2.s
+++ bootloader/stage2.s
@@ -43,10 +43,75 @@ jmp $
 .done:
 mov si, hello
 call puts
+
+; next we will verify that a20 line is enabled before jumping to protected mode
+call check_a20
+cmp ax, 1                          ; ax = 1 if a20 is enabled
+jne a20_disabled_error             ; TODO - handle case when a20 is not enabled
+
 jmp $
+
+; poll if a20 line is enabled
+; inputs - none
+; ax = 1 if a20 is enabled, otherwise ax = 0
+; routine adopted from https://wiki.osdev.org/A20_Line
+check_a20:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+ 
+    cli
+ 
+    xor ax, ax                     ; ax = 0
+    mov es, ax
+ 
+    not ax                         ; ax = 0xffff
+    mov ds, ax
+ 
+    mov di, 0x0500
+    mov si, 0x0510
+ 
+    mov al, byte [es:di]
+    push ax
+ 
+    mov al, byte [ds:si]
+    push ax
+ 
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xff
+ 
+    cmp byte [es:di], 0xff
+ 
+    pop ax
+    mov byte [ds:si], al
+ 
+    pop ax
+    mov byte [es:di], al
+ 
+    mov ax, 0
+    je .exit
+ 
+    mov ax, 1
+ 
+    .exit:
+    pop si
+    pop di
+    pop es
+    pop ds
+    popf
+ 
+    ret
 
 hello: db 'hello from stage2!', 0
 memory_map_error: db 'error detecting memory map', 0
+a20_not_enabled: db 'a20 is not enabled!', 0
+
+a20_disabled_error:
+    mov si, a20_not_enabled
+    call puts
+    jmp $
 
 ; prints a string to the screen
 ; si - pointer to string to be printed
