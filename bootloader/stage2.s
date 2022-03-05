993e3e8c7748d9f4b4a48b916267d4657f3b42db
Sam Kravitz <samkravitz99@gmail.com>
2022-03-04 23:48:20 -0500

    [bootloader] Enter protected mode

--- bootloader/stage2.s
+++ bootloader/stage2.s
@@ -8,7 +8,9 @@
 ; DESCRIPTION: stage 2 bootloader
 
 [bits 16]
-org 0x5000    ; we are expecting stage1 to load us to memory address 0x5000
+org 0x5000                     ; we are expecting stage1 to load us to memory address 0x5000
+
+mov sp, 5000h                  ; stack now points to here
 
 ; detect memory map
 ; we will store the generated list at address 0x4000 (es:di => 0000:4000)
@@ -41,15 +43,61 @@ call puts
 jmp $
 
 .done:
-mov si, hello
-call puts
 
 ; next we will verify that a20 line is enabled before jumping to protected mode
 call check_a20
-cmp ax, 1                          ; ax = 1 if a20 is enabled
-jne a20_disabled_error             ; TODO - handle case when a20 is not enabled
+cmp ax, 1                      ; ax = 1 if a20 is enabled
+jne a20_disabled_error         ; TODO - handle case when a20 is not enabled
+
+; now we know a20 is enabled, we can get ready to enter protected mode
+cli
+lgdt [gdt_descriptor]          ; load gdt
+mov eax, cr0                   ; eax = cr0
+or eax, 1                      ; enable protected mode
+mov cr0, eax                   ; cr0 = eax
+
+jmp 0x08:pmode                 ; jump to protected mode!
+
+; 32 bit protected mode
+pmode:
+[bits 32]
+; set up new segments
+mov ax, 10h
+mov ds, ax
+mov ss, ax
 
 jmp $
+
+; tell nasm remainder of this file is 16 bit mode
+[bits 16]
+
+; initialize gdt
+gdt:
+gdt_null:           ; null descriptor
+    dd 0x0			; 4 bytes of 0
+    dd 0x0       	; 4 bytes of 0
+
+gdt_code:			; code segment descriptor
+	dw 0xffff		; limit (bits 0-15)
+	dw 0x0			; base  (bits 0-15)
+	db 0x0			; base  (bits 16-23)
+	db 10011010b	; flags
+	db 11001111b	; flags cont., limit (bits 16-19)
+	db 0x0			; base (bits 24-31)
+
+gdt_data:			; data segment descriptor
+	dw 0xffff		; limit (bits 0-15)
+	dw 0x0			; base (bits 0-15)
+	db 0x0			; base (bits 16-23)
+	db 10010010b	; flags
+	db 11001111b	; flags cont., limit (bits 16-19)
+	db 0x0			; base (bits 24-31)
+gdt_end:
+
+; 6 byte value to be stored in gdtr
+gdt_descriptor:
+dw gdt_end - gdt - 1	; size of gdt minus 1
+dd gdt					; starting address of GDT
 
 ; poll if a20 line is enabled
 ; inputs - none
@@ -104,10 +152,6 @@ check_a20:
  
     ret
 
-hello: db 'hello from stage2!', 0
-memory_map_error: db 'error detecting memory map', 0
-a20_not_enabled: db 'a20 is not enabled!', 0
-
 a20_disabled_error:
     mov si, a20_not_enabled
     call puts
@@ -124,3 +168,8 @@ puts:
 	jmp puts
 .done:
 	ret
+
+hello: db 'hello from stage2!', 0
+memory_map_error: db 'error detecting memory map', 0
+a20_not_enabled: db 'a20 is not enabled!', 0
+welcome_pmode: db 'welcome to protected mode!', 0
