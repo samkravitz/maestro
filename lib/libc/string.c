8777f30d12d1060915a40a3483487b1b5da4a816
Sam Kravitz <samkravitz99@gmail.com>
2022-03-08 14:57:43 -0500

    [libc] Implment strtok

--- lib/libc/string.c
+++ lib/libc/string.c
@@ -164,3 +164,40 @@ size_t strlen(const char *str)
 
 	return len;
 }
+
+char *strtok(char *str, const char *delim)
+{
+    // buffer to persist str between calls
+    static char *save;
+
+    if (!str)
+        str = save;
+    
+    if (!str || strlen(str) == 0)
+        return NULL;
+
+    size_t delim_len = strlen(delim);
+
+    // check if str begins with delim
+    if (strncmp(str, delim, delim_len) == 0)
+    {
+        str += delim_len;
+        if (!str)
+            return NULL;
+    }
+
+    char *ret = str;
+
+    for (; *str; str++)
+    {
+        if (strncmp(str, delim, delim_len) == 0)
+        {
+            *str = '\0';
+            save = str + 1;
+            return ret;
+        }
+    }
+
+    save = NULL;
+    return ret;
+}
--- lib/libc/string.h
+++ lib/libc/string.h
@@ -24,5 +24,6 @@ int strcmp(const char *, const char *);
 int strncmp(const char *, const char *, size_t);
 char *strcpy(char *, const char *);
 size_t strlen(const char *);
+char *strtok(char *, const char *);
 
 #endif    // STRING_H
