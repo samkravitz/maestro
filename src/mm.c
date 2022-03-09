6350cfabe816fd279daa14b01051b156d43083cc
Sam Kravitz <samkravitz99@gmail.com>
2022-03-08 22:53:47 -0500

    [mm] Remove multiboot structures

--- src/mm.c
+++ src/mm.c
@@ -19,9 +19,6 @@
 #define MMAP_CLEAR(bit) (meminfo.mmap[bit / 8] &= ~(1 << (bit % 8)))
 #define MMAP_TEST(bit)  (meminfo.mmap[bit / 8] & (1 << (bit % 8)))
 
-// address of multiboot information struct
-extern uptr mboot_info;
-
 struct meminfo meminfo;
 
 u32 *kpd;    // kernel page directory
@@ -81,69 +78,6 @@ void mminit()
 {
 	// register page fault exception handler
 	svect(14, pfault);
-
-	struct mboot_info *info = (struct mboot_info *) mboot_info;
-	meminfo.size            = info->mem_upper;
-	meminfo.max_blocks      = meminfo.size * 1024 / PAGE_SIZE;
-	meminfo.used_blocks     = meminfo.max_blocks;    // by default all blocks are used
-	meminfo.mmap            = (u8 *) kmalloc(meminfo.max_blocks / 8);
-	memset(meminfo.mmap, 0, meminfo.max_blocks / 8);
-
-	// get memory map if valid
-	if (info->flags >> 6 & 1)
-	{
-		kprintf("maestro physical memory map:\n");
-		/*
-		 *	Each multiboot mmap entry is stored as the following:
-		 *	0 - size
-		 *	4 - base_addr_low
-		 *	8 - base_addr_high
-		 *	12 - length_low
-		 *	16 - length_high
-		 *	20 - type
-		 */
-		struct multiboot_mmap_entry
-		{
-			u32 size;
-			u32 addr_low;
-			u32 addr_high;
-			u32 len_low;
-			u32 len_high;
-			u32 type;
-#define MULTIBOOT_MEMORY_AVAILABLE        1
-#define MULTIBOOT_MEMORY_RESERVED         2
-#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE 3
-#define MULTIBOOT_MEMORY_NVS              4
-#define MULTIBOOT_MEMORY_BADRAM           5
-		};
-
-		uint len                           = 0;
-		struct multiboot_mmap_entry *entry = (struct multiboot_mmap_entry *) info->mmap_addr;
-		while (len < info->mmap_length)
-		{
-			char typestr[32];
-			switch (entry->type)
-			{
-				case MULTIBOOT_MEMORY_AVAILABLE:
-					strcpy(typestr, "Available");
-					break;
-				case MULTIBOOT_MEMORY_ACPI_RECLAIMABLE:
-					strcpy(typestr, "ACPI Reclaimable");
-					break;
-				case MULTIBOOT_MEMORY_NVS:
-					strcpy(typestr, "NVS");
-					break;
-				case MULTIBOOT_MEMORY_BADRAM:
-					strcpy(typestr, "Defective RAM");
-					break;
-				default:
-					strcpy(typestr, "Reserved");
-			}
-			kprintf("0x%8x-0x%8x:	%s\n", entry->addr_low, entry->addr_low + entry->len_low - 1, typestr);
-			entry++;
-			len += sizeof(struct multiboot_mmap_entry);
-		}
-	}
 
 	// kpd = (u32 *) kmalloca(sizeof(struct pagedir));
 	// memset(kpd, 0, sizeof(kpd));
