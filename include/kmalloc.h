4a1d96f7fc9cda311d3884bc8a3e66e17deb4420
Sam Kravitz <samkravitz99@gmail.com>
2022-03-14 12:42:00 -0400

    [kmalloc] Add data structures for freelist malloc implementation

--- include/kmalloc.h
+++ include/kmalloc.h
@@ -1,28 +1,124 @@
+/* maestro
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * Authors: Noah Bennett & Sam Kravitz
+ *
+ * FILE: kmalloc.h
+ * DATE: March 14th, 2022
+ * DESCRIPTION: Implentation of a kernel heap
+ */
 #ifndef KMALLOC_H
 #define KMALLOC_H
 
 #include <maestro.h>
 
-// page size
-#define _PAGE_SIZE 4096
+// minimum size in bytes that can be kmalloc'd
+#define MIN_ALLOCATION    8
 
-struct mem_block
+// size of the header for an allocated block
+#define ALLOC_HEADER_SIZE (sizeof(struct header) - (2 * sizeof(struct header *)))
+
+/**
+ * @brief represents the allocation state of an object
+ */
+enum state
 {
-	size_t size;
-	u8 free;
-	struct mem_block *next;
-	struct mem_block *prev;
-	int debug;    // delete me
+	UNALLOCATED = 0,
+	ALLOCATED   = 1,
+	SENTINAL    = 2,
 };
+
+/**
+ * @brief contains metadata about a heap object
+ * 
+ * objects must be a multiple of 8 bytes, which means the size of the object
+ * will always end in 0b00. Therefore we can use these bottom 2 bits
+ * to encode some information about the allocation state of the object.
+ * 
+ * additionally, we use a union because different metadata is needed depending on whether an
+ * object is free or allocated. When allocated, data points to the first byte of data of the
+ * object (i.e, the ptr returned to the user by malloc). When unallocated, we instead keep 
+ * pointers to the next and previous object in the free list
+ */
+struct header
+{
+	// encoding of the object's size & state
+	size_t size_state;
+
+	// size in bytes of the object to the left of this
+	size_t left_size;
+	union
+	{
+		// used when the object is free
+		struct
+		{
+			struct header *next;    // ptr to next unallocated object
+			struct header *prev;    // ptr to previous unallocated object
+		};
+
+		// used when the object is allocated
+		u8 data[0];
+	};
+};
+
+// helper functions to work with headers
+static inline size_t get_block_size(struct header *h)
+{
+	return h->size_state & ~0x3;
+}
+
+static inline enum state get_block_state(struct header *h)
+{
+	return (enum state) (h->size_state & 0x3);
+}
+
+static inline void set_block_size(struct header *h, size_t size)
+{
+	h->size_state = size | (h->size_state & 0x3);
+}
+
+static inline void set_block_state(struct header *h, enum state s)
+{
+	h->size_state = (h->size_state & ~0x3) | s;
+}
+
+/**
+ * @brief get a header pointer from a pointer and an offset
+ * @param ptr base pointer
+ * @param off number of bytes from base pointer where header is located
+ * @return a pointer to a header offset bytes from pointer
+ */
+static inline struct header *get_header_from_offset(void *ptr, ptrdiff_t off)
+{
+	return (struct header *) ((char *) ptr + off);
+}
+
+/**
+ * @brief gets the header to the right of a given header
+ * @param h original header
+ * @return header to the right of h
+ */
+static inline struct header *get_right_header(struct header *h)
+{
+	return get_header_from_offset(h, get_block_size(h));
+}
+
+/**
+ * @brief gets the header to the left of a given header
+ * @param h original header
+ * @return header to the left of h
+ */
+inline static struct header *get_left_header(struct header *h)
+{
+	return get_header_from_offset(h, -h->left_size);
+}
 
 void kmalloc_init(void *, size_t);
 
-void *brk(int);
-void kfree(void *);
 void *kmalloc(size_t);
+void kfree(void *);
 
-struct mem_block *find_next_free(struct mem_block **, size_t);
-struct mem_block *request_memory(struct mem_block *, size_t);
-struct mem_block *get_block_ptr(void *);
+void print_heap();
+void print_freelist();
 
-#endif    // KMALLOC
+#endif    // KMALLOC_H
--- src/kmalloc.c
+++ src/kmalloc.c
@@ -1,188 +1,67 @@
+/* maestro
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * Authors: Noah Bennett & Sam Kravitz
+ *
+ * FILE: kmalloc.c
+ * DATE: March 14th, 2022
+ * DESCRIPTION: Implentation of a kernel heap
+ */
 #include <kmalloc.h>
 #include <kprintf.h>
 
-#include "string.h"
+// freelist sentinal - denotes the head of the doubly-linked freelist
+static struct header *sentinal;
 
 // pointers to the base of the heap, and current top
 void *base, *heap;
 
-// length of heap in bytes
-static size_t len;
-
-void *block_head = NULL;
+// rounds an number x up to the nearest multiple of 8
+#define round8(x) ((x + 7) & ~0x7);
 
 void kmalloc_init(void *p, size_t s)
 {
-	base = heap = p;
-	len = s;
-}
+    base = p;
+	heap = base + s;
 
-void *brk(int amt)
-{
-	void *p = heap;
-	heap += amt;
+	// initialize the freelist
+	sentinal = (struct header *) p;
+	set_block_size(sentinal, sizeof(struct header));
+	set_block_state(sentinal, SENTINAL);
 
-	// we need to request more memory from vmm instead of just expanding heap
-	if (heap > base + len)
-	{
-		kprintf("Error: need to expand heap!\n");
-		while (1)
-			;
-	}
+	// first unallocated object
+	struct header *h = base + sizeof(struct header);
+	set_block_size(h, s - sizeof(struct header));
+	set_block_state(h, UNALLOCATED);
 
-	return p;
-}
+	sentinal->next = h;
+	sentinal->prev = h;
+	sentinal->left_size = 0;
 
-/**
- * set a block of memory as free as in libre
- * @param ptr pointer to the memory block to mark as free
- */
-void kfree(void *ptr)
-{
-	// ignore null pointers :o
-	if (!ptr)
-		return;
+	h->next = sentinal;
+	h->prev = sentinal;
+	h->left_size = get_block_size(sentinal);
 
-	// get memory block that is being freed
-	struct mem_block *freeing_block_ptr = get_block_ptr(ptr);
-
-	// return if block is already free
-	if (freeing_block_ptr->free)
-		return;
-
-	// assign block as free
-	// debug will be 0xBAADF00D if successfully freed
-	// the block previous to the block that is being freed
-	// is assigned to the next block to prevent 'gaps' in the
-	// memory that is available
-	freeing_block_ptr->free  = 1;
-	freeing_block_ptr->debug = 0xBAADF00D;
-
-	if (freeing_block_ptr->prev)
-		freeing_block_ptr->prev->next = freeing_block_ptr->next;
+	(void) print_heap;
+	(void) print_freelist;
 }
 
 /**
- * kernel malloc
- * @param size malloc size in n bytes
+ * @brief allocates memory on the kernel's heap
+ * @param size the size in bytes of the request
+ * @return pointer to the first usable data byte of the request
  */
 void *kmalloc(size_t size)
 {
-	struct mem_block *i_am;
-	// todo: handle alignment with page
-	// actually i thhink it works with kmalloca
-
-	// if someone is playing games... return null
-	if (size <= 0)
-		return NULL;
-
-	if (!block_head)
-	{
-		// if head is undefined, make request for memory
-		// if denied return null, otherwise we know
-		// the memory block head is defined
-		i_am = request_memory(NULL, size);
-		if (!i_am)
-			return NULL;
-	}
-
-	else
-	{
-		// keep previous block head, next block head is
-		// defined when finding the next free
-		struct mem_block *was = block_head;
-		i_am                  = find_next_free(&was, size);
-
-		if (!i_am)
-		{
-			// request more mem if no free blocks were found
-			i_am = request_memory(was, size);
-
-			// if request from was was denied return null
-			if (!i_am)
-				return NULL;
-		}
-
-		else
-		{
-			// successfully found a free block
-			i_am->free  = 0;
-			i_am->debug = 0xBAD1DEED;
-			i_am->prev  = was;
-		}
-	}
-
-	// return the available memory
-	return ++i_am;
+	(void) size;
+	return NULL;
 }
 
 /**
- * find the next available memory block, if one exists
- * @param was double pointer to previous memory block
- * @param size size that is currently trying to be malloc'd 
+ * @brief deallocates memory from the kernel's heap
+ * @param p pointer to user's data that was returned by kmalloc
  */
-struct mem_block *find_next_free(struct mem_block **was, size_t size)
+void kfree(void *p)
 {
-	struct mem_block *i_am = block_head;
-	int i                  = 0;
-	// iterate through the list until free mem is found
-	for (;;)
-	{
-		if (i_am && i_am->free && i_am->size >= size)
-		{
-			// if block is not null, and the block is free, and
-			// the block is of a valid size, correct block
-			// has been found
-			break;
-		}
-
-		*was = i_am;
-		i_am = i_am->next;
-		i++;
-	}
-
-	// return the found free block
-	return i_am;
-}
-
-/**
- * ask `brk` for more memory
- * @param was pointer to the previous memory block
- * @param size n bytes to request
- */
-struct mem_block *request_memory(struct mem_block *was, size_t size)
-{
-	struct mem_block *block;
-	block               = brk(0);
-	void *requested_mem = brk(sizeof(*block) + size);
-
-	// failed to fetch more memory
-	if (requested_mem == (void *) -1)
-		return NULL;
-
-	// was will always be null on the first request
-	// because we're starting with the head of the list
-	if (was)
-		was->next = block;
-
-	// prepend this block to the head of the heap and
-	// append the new tail of the list to this block
-	block->next  = NULL;
-	block->prev  = was;
-	block->size  = size;
-	block->free  = 0;
-	block->debug = 0xBADDDD1E;
-	return block;
-}
-
-/**
- * get pointer to the block of memory
- * just does pointer - 1 but is more semantic / easier
- * to read than pointer arithmetic and casting to struct
- * each time
- * @param ptr pointer to get block of memory for
- */
-struct mem_block *get_block_ptr(void *ptr)
-{
-	return ((struct mem_block *) ptr) - 1;
+	(void) p;
 }
