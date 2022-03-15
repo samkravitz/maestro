3012f3bb40b0b62c669d0f893c78012dac4113c2
Sam Kravitz <samkravitz99@gmail.com>
2022-03-15 19:19:46 -0400

    [kmalloc] Remove some unused utility functions

--- include/kmalloc.h
+++ include/kmalloc.h
@@ -18,9 +18,6 @@ struct mem_block
 void *brk(int);
 void kfree(void *);
 void *kmalloc(size_t);
-void *kmallocp(size_t, u32 *);
-void *krealloc(void *, size_t);
-void *kcalloc(size_t, size_t);
 
 struct mem_block *find_next_free(struct mem_block **, size_t);
 struct mem_block *request_memory(struct mem_block *, size_t);
