01c4ad1a423770b31ab47db8ec11a20ef2930c3f
Sam Kravitz <samkravitz99@gmail.com>
2022-03-22 12:23:18 -0400

    [kbd] Print key presses to screen instead of serial console

--- src/kbd.c
+++ src/kbd.c
@@ -9,10 +9,9 @@
  */
 #include <kbd.h>
 
-#include <intr.h>
 #include <io.h>
-#include <kprintf.h>
 
+#include "stdio.h"
 #include "stdlib.h"
 
 // state of special characters
@@ -24,35 +23,30 @@ void kbdhandler()
 {
 	u8 scancode = inb(KBD_IN);
 
-	char c      = '\0';
-	if (scancode < NUM_KEYS)
-		c = kbdus[scancode];
+	if (scancode >= NUM_KEYS)
+		return;
 
+	char c = kbdus[scancode];
+
+	// check for special keys that could alter key state, like shifts
 	switch (scancode)
 	{
 		// left shift pressed
-		case LSHIFT_IDX:
-			state |= LSHIFT;
-			break;
+		case LSHIFT_IDX:        state |= LSHIFT;  break;
 
 		// left shift released
-		case LSHIFT_IDX + 0x80:
-			state &= ~LSHIFT;
-			break;
+		case LSHIFT_IDX + 0x80: state &= ~LSHIFT; break;
 
 		// right shift pressed
-		case RSHIFT_IDX:
-			state |= RSHIFT;
-			break;
+		case RSHIFT_IDX:        state |= RSHIFT;  break;
 
 		// right shift released
-		case RSHIFT_IDX + 0x80:
-			state &= ~RSHIFT;
-			break;
+		case RSHIFT_IDX + 0x80: state &= ~RSHIFT; break;
 	}
 
 	if (is_alpha(c) && PRESSED(LSHIFT | RSHIFT))
 		c = to_upper(c);
 
-	kprintf("%c", c);
+	// TODO - this should be buffered so it can be read from stdin
+	printf("%c", c);
 }
