75fb8906ea4fac99aedd421b132c3b4fed50615a
Sam Kravitz <samkravitz99@gmail.com>
2022-03-22 17:28:02 -0400

    [kmalloc] Implement linked list kmalloc

--- src/kmalloc.c
+++ src/kmalloc.c
@@ -53,7 +53,72 @@ void kmalloc_init(void *p, size_t s)
  */
 void *kmalloc(size_t size)
 {
-	(void) size;
+	if (size == 0)
+		return NULL;
+
+	// total size of object in bytes, includes user request and metadata
+	size_t total_size = size;
+
+	if (total_size < MIN_ALLOCATION)
+		total_size = MIN_ALLOCATION;
+
+	total_size = round8(total_size);
+	total_size += ALLOC_HEADER_SIZE;
+
+	// loop through freelist to find a block big enough for request
+	struct header *block = sentinal->next;
+	while (block != sentinal)
+	{
+		// block is big enough for our request
+		if (get_block_size(block) >= total_size)
+		{
+			// get remaining size of this block after request has been served
+			size_t rem = get_block_size(block) - total_size;
+
+			// block can be split into 2 allocable blocks
+			if (rem >= MIN_ALLOCATION + ALLOC_HEADER_SIZE)
+			{
+				// split the block into a right and left block
+				// return to the user the block higher in memory (right block)
+
+				// left block
+				struct header *left = block;
+				set_block_size(left, rem);
+
+				// right block (the one being returned to the user)
+				struct header *right = get_right_header(block);
+				set_block_size(right, total_size);
+				set_block_state(right, ALLOCATED);
+				right->left_size = get_block_size(left);
+
+				// on most splits, the block to the right of the right block
+				// will need to have its left_size adjusted
+				struct header *right_right = get_right_header(right);
+				if ((uintptr_t) right_right < (uintptr_t) heap)
+					right_right->left_size = get_block_size(right);
+
+				return (void *) right->data;
+			}
+
+			// block cannot be split into 2 allocable blocks
+			// simply remove the block from the freelist
+			else
+			{
+				block->prev->next = block->next;
+				block->next->prev = block->prev;
+				set_block_state(block, ALLOCATED);
+
+				return (void *) block->data;
+			}
+		}
+
+		block = block->next;
+	}
+
+	// no block in freelist was big enough to accomodate our request,
+	// so we have to expand the heap
+	kprintf("Time to expand the heap!\n");
+
 	return NULL;
 }
 
@@ -64,4 +129,42 @@ void *kmalloc(size_t size)
 void kfree(void *p)
 {
 	(void) p;
+}
+
+static const char *state_strings[] = {
+	"UNALLOCATED",
+	"ALLOCATED",
+	"SENTINAL",
+};
+
+void print_heap()
+{
+	kprintf("\tHEAP: \n");
+	struct header *h = base;
+
+	while ((uintptr_t) h < (uintptr_t) heap)
+	{
+		kprintf("addr: 0x%x\n", h);
+		kprintf("size: %d (%xh)\n", get_block_size(h), get_block_size(h));
+		kprintf("state: %s\n", state_strings[get_block_state(h)]);
+		kprintf("left size: %d (%xh)\n\n", h->left_size, h->left_size);
+
+		h = get_right_header(h);
+	}
+}
+
+void print_freelist()
+{
+	kprintf("\tFREELIST: \n");
+	struct header *freelist = sentinal;
+
+	do
+	{
+		kprintf("addr: 0x%x\n", freelist);
+		kprintf("size: %d (%xh)\n", get_block_size(freelist), get_block_size(freelist));
+		kprintf("prev: 0x%x\n", freelist->prev);
+		kprintf("next: 0x%x\n\n", freelist->next);
+
+		freelist = freelist->next;
+	} while (freelist != sentinal);
 }
