1cbe360b7ce8ea50f623b4e04036f188dcba5827
Sam Kravitz <samkravitz99@gmail.com>
2022-03-28 17:55:33 -0400

    [gdt] Add user mode data/code segments

--- src/start.s
+++ src/start.s
@@ -46,7 +46,8 @@ gdt_null:                  ; null descriptor
 	dd 0                   ; 4 bytes of 0
 	dd 0                   ; 4 bytes of 0
 
-gdt_code:                  ; code segment descriptor
+; kernel segments
+gdt_kcode:                 ; kernel mode code segment
 	dw 0ffffh              ; limit (bits 0-15)
 	dw 0                   ; base  (bits 0-15)
 	db 0                   ; base  (bits 16-23)
@@ -54,13 +55,31 @@ gdt_code:                  ; code segment descriptor
 	db 11001111b           ; flags cont., limit (bits 16-19)
 	db 0                   ; base (bits 24-31)
 
-gdt_data:                  ; data segment descriptor
+gdt_kdata:                 ; kernel mode data segment
 	dw 0ffffh              ; limit (bits 0-15)
 	dw 0                   ; base (bits 0-15)
 	db 0                   ; base (bits 16-23)
 	db 10010010b           ; flags
 	db 11001111b           ; flags cont., limit (bits 16-19)
 	db 0                   ; base (bits 24-31)
+
+; user segments
+gdt_ucode:                 ; user mode code segment
+	dw 0ffffh              ; limit (bits 0-15)
+	dw 0                   ; base  (bits 0-15)
+	db 0                   ; base  (bits 16-23)
+	db 11111010b           ; flags
+	db 11001111b           ; flags cont., limit (bits 16-19)
+	db 0                   ; base (bits 24-31)
+
+gdt_udata:                 ; user mode data segment
+	dw 0ffffh              ; limit (bits 0-15)
+	dw 0                   ; base (bits 0-15)
+	db 0                   ; base (bits 16-23)
+	db 11110010b           ; flags
+	db 11001111b           ; flags cont., limit (bits 16-19)
+	db 0                   ; base (bits 24-31)
+
 gdt_end:
 
 ; 6 byte value to be stored in gdtr
