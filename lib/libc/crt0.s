fcf4092928f04cd8587a0a506d0b20ad98d6affb
Sam Kravitz <samkravitz99@gmail.com>
2022-04-27 19:00:23 -0400

    [libc] Add crt0.s

---
+++ lib/libc/crt0.s
@@ -0,0 +1,21 @@
+; maestro
+; License: GPLv2
+; See LICENSE.txt for full license text
+; Author: Sam Kravitz
+;
+; FILE: libc/crt0.s
+; DATE: April 27th, 2022
+; DESCRIPTION: defines _start symbol for userspace processes
+[bits 32]
+
+	global _start
+	extern main
+
+	section .text
+
+_start:
+	xor ebx, ebx
+	push ebx
+	mov ebp, esp
+
+	call main
