5865c0fe1d1f394936d0b279df8a393718761b34
Sam Kravitz <samkravitz99@gmail.com>
2023-03-13 18:25:10 -0500

    [msh] Add super basic shell

---
+++ user/msh/msh.c
@@ -0,0 +1,43 @@
+/* maestro
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * Author: Sam Kravitz
+ *
+ * FILE: user/msh/msh.c
+ * DATE: March 13th, 2023
+ * DESCRIPTION: maestro shell
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+char line[1024];
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+
+	while (1)
+	{
+		memset(line, 0, 1024);
+        printf("> ");
+        int pos = 0;
+
+        while (1)
+        {
+            int c = getc();
+            if (c == '\n')
+                break;
+            
+            line[pos++] = c;
+            printf("%c", c);
+        }
+
+        if (!strcmp("exit", line))
+            break;
+	}
+
+	return 0;
+}
