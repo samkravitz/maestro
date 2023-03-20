db107122238c0ef93741b83b30e03da8283af9ba
Sam Kravitz <samkravitz99@gmail.com>
2023-03-20 07:17:35 -0500

    [libc] Add dirent.h And opendir()

--- lib/libc/include/fcntl.h
+++ lib/libc/include/fcntl.h
@@ -1,15 +1,17 @@
 #ifndef FCNTL_H
 #define FCNTL_H
 
-#define O_RDONLY   0x1
-#define O_WRONLY   0x2
-#define O_RDWR     0x4
-#define O_APPEND   0x8
-#define O_CREAT    0x10
-#define O_DSYNC    0x20
-#define O_EXCL     0x40
-#define O_NOTTY    0x80
-#define O_NONBLOCK 0x100
+#define O_RDONLY    0x1
+#define O_WRONLY    0x2
+#define O_RDWR      0x4
+#define O_APPEND    0x8
+#define O_CREAT     0x10
+#define O_DSYNC     0x20
+#define O_EXCL      0x40
+#define O_NOTTY     0x80
+#define O_NONBLOCK  0x100
+#define O_DIRECTORY 0x200
+#define O_CLOEXEC   0x400
 
 int open(const char *, int, ...);
 
