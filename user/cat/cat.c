ed8dd33c05587394cc10a32a0993a9d71978f350
Sam Kravitz <samkravitz99@gmail.com>
2025-12-12 22:00:44 -0600

    [cat] Add cat user program

---
+++ user/cat/cat.c
@@ -0,0 +1,27 @@
+/* maestro
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * Author: Sam Kravitz
+ *
+ * FILE: user/cat/cat.c
+ * DATE: December 12, 2025
+ * DESCRIPTION: cat - concatenate and display files
+ */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Usage: cat <file>\n");
+		return 1;
+	}
+
+	int fd = open(argv[1], O_RDONLY);
+	char buffer[1024];
+	read(fd, buffer, 1024);
+	printf("%s\n", buffer);
+}
