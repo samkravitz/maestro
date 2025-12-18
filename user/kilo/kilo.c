415c627f706597f68cc7bdd57fa4273827236fd0
Sam Kravitz <samkravitz99@gmail.com>
2025-12-18 03:58:25 -0600

    [user] Start kilo text editor https://viewsourcecode.org/snaptoken/kilo/index.html

---
+++ user/kilo/kilo.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
+  return 0;
+}
